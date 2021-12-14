440e18660955c69b2b75ed835e2a8c1e061d6a3b
lkguo <lkguo2@iflytek.com>
2021-12-14 17:00:01 +0800

    修改404页面样式，添加动画

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="page-404">
     404Page
   </div>
 </template>
@@ -11,5 +11,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .page-404 {
+    /* width: 100wh; */
+    height: 100vh;
+    background-color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: my-animation 5s infinite;
+  }
+  @keyframes my-animation {
+    0% {
+      background-color: #fff;
+      color: #000;
+    }
+    50% {
+      background-color: #000;
+      color: #fff;
+    }
+    100% {
+      background-color: #fff;
+      color: #000;
+    }
+  }
 </style>
