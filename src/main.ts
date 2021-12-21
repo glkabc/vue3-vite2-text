611a0aa3672b0e68ff4b393be7b791f2553626fe
lkguo <lkguo2@iflytek.com>
2021-12-21 14:48:09 +0800

    添加提交时代码格式化工具

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import naive from '@/config/naiveUI'
+import { createApp } from "vue";
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
+import naive from "@/config/naiveUI";
 
-
-createApp(App)
-    .use(router)
-    .use(store)
-    .use(naive)
-    .mount('#app')
+createApp(App).use(router).use(store).use(naive).mount("#app");
